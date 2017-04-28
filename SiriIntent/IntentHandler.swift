f46e4a972e95489558551bb54031eb946a7894c9
chetlq <admin@Mourats-Mac-mini.local>
2017-04-28 20:01:02 +0300

    22
    22

--- SiriIntent/IntentHandler.swift
+++ SiriIntent/IntentHandler.swift
@@ -7,6 +7,7 @@
 //
 
 import Intents
+import Foundation
 
 // As an example, this class is set up to handle Message intents.
 // You will want to replace this or add other intents as appropriate.
@@ -30,43 +31,111 @@ class IntentHandler: INExtension{
   
 }
 
+
+func inPerson(tel:String, name:String)-> INPerson {
+    let nameFormatter = PersonNameComponentsFormatter()
+    let personHandle = INPersonHandle(value: tel, type: INPersonHandleType.phoneNumber)
+    
+    if let nameComponents = nameFormatter.personNameComponents(from: name){
+        return INPerson(personHandle: personHandle, nameComponents: nameComponents, displayName: nameComponents.familyName, image: nil, contactIdentifier: nil, customIdentifier: nil)
+    }else{
+        return INPerson(personHandle: personHandle, nameComponents: nil, displayName: nil, image: nil, contactIdentifier: nil, customIdentifier: nil)
+        
+    }
+    
+}
+
 class SendMoneyIntentHandler: NSObject, INSendPaymentIntentHandling{
     public func handle(sendPayment intent: INSendPaymentIntent, completion: @escaping (INSendPaymentIntentResponse) -> Void) {
         if let person = intent.payee, let amount = intent.currencyAmount{
             completion(INSendPaymentIntentResponse.init(code: .success, userActivity: nil))
         }else{
-            completion(INSendPaymentIntentResponse.init(code: .success, userActivity: nil))
+            completion(INSendPaymentIntentResponse.init(code: .failure, userActivity: nil))
         }
     }
     
     public func resolvePayee(forSendPayment intent: INSendPaymentIntent, with completion: @escaping (INPersonResolutionResult) -> Swift.Void){
         if let payee = intent.payee {
-            let contacts:[PaymentContact] = PaymentContact.allContacts()
-            var resolutionResult: INPersonResolutionResult?
-            var matchedContacts: [PaymentContact] = []
             
-            for contact in contacts {
-                print("check exlist\(contact.name) vs \(payee.displayName)")
-                
-                if contact.name == payee.displayName{
-                    matchedContacts.append(contact)
+            
+            
+
+            
+            
+            
+            
+            let url = URL(string: "https://sbertech.herokuapp.com/users")
+            var val   = [String]()
+            
+            URLSession.shared.dataTask(with: url!)
+            {
+                data, response, error in guard error == nil else {
+                    print(error!)
+                    return
                 }
-            }
+                guard let data = data else {
+                    print("Data is empty")
+                    return
+                }
+                
+                var name: String
+                let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [[String:Any]]
+                
+                for item in json {
+                    name = item["name"] as! String
+                    
+                    //val.append(name)
+                    print(item["name"] as! String) // cast directly to String
+                }
+                //print("Count is \(val.count).")
+                
+                
+                
+                //let contacts:[PaymentContact] = PaymentContact.allContacts()
+                var resolutionResult: INPersonResolutionResult?
+                var matchedContacts: [INPerson] = []
+                
+                for contact in json {
+                    let name = contact["name"] as! String
+                    print("check exlist\(name) vs \(payee.displayName)")
+                    
+                    if name == payee.displayName{
+                        val.append(name)
+                        matchedContacts.append(inPerson(tel: "123", name: name))
+                    }
+                }
+                
+                switch val.count {
+                case 0:
+                    print("nothing matched")
+                    resolutionResult = INPersonResolutionResult.unsupported()
+                case 1:
+                    print("best matched")
+                    let recipientMatched = inPerson(tel: "123", name: val[0])
+                    resolutionResult = INPersonResolutionResult.success(with: recipientMatched)
+                default:
+                    print("more then one matched")
+                    let disambiquationOptions:[INPerson] = matchedContacts.map{ contact in return contact.inPerson()}
+                    resolutionResult = INPersonResolutionResult.disambiguation(with: disambiquationOptions)
+                }
+                completion(resolutionResult!)
+                
+                
+                
+
+                
+                
+                }.resume()
             
-            switch matchedContacts.count {
-            case 0:
-                print("nothing matched")
-                resolutionResult = INPersonResolutionResult.unsupported()
-            case 1:
-                print("best matched")
-                let recipientMatched = matchedContacts[0].inPerson()
-                resolutionResult = INPersonResolutionResult.success(with: recipientMatched)
-            default:
-                print("more then one matched")
-                let disambiquationOptions:[INPerson] = matchedContacts.map{ contact in return contact.inPerson()}
-                resolutionResult = INPersonResolutionResult.disambiguation(with: disambiquationOptions)
-            }
-            completion(resolutionResult!)
+            
+            
+            
+            
+
+            
+            
+            
+            
         }else{
             completion(INPersonResolutionResult.needsValue())
         }
--- siriProject/PaymentContact.swift
+++ siriProject/PaymentContact.swift
@@ -11,24 +11,24 @@ import Intents
 
 class PaymentContact: NSObject {
     let name:String
-    let email:String
+    let tel:String
     
-    init(name: String, email: String){
+    init(name: String, tel: String){
         self.name = name
-        self.email = email
+        self.tel = tel
     }
-    
-    class func allContacts() -> [PaymentContact]{
-        return [
-            PaymentContact(name: "Anber", email: "amber@apple.com"),
-            PaymentContact(name: "Sheldom", email: "sheldom@apple.com"),
-            PaymentContact(name: "Freeman", email: "freeman@apple.com")
-        ]
-    }
+//    
+//    class func allContacts() -> [PaymentContact]{
+//        return [
+//            PaymentContact(name: "Александр", email: "amber@apple.com"),
+//            PaymentContact(name: "Sheldom", email: "sheldom@apple.com"),
+//            PaymentContact(name: "Freeman", email: "freeman@apple.com")
+//        ]
+//    }
     
     func inPerson()-> INPerson {
         let nameFormatter = PersonNameComponentsFormatter()
-        let personHandle = INPersonHandle(value: self.email, type: INPersonHandleType.emailAddress)
+        let personHandle = INPersonHandle(value: self.tel, type: INPersonHandleType.phoneNumber)
         
         if let nameComponents = nameFormatter.personNameComponents(from: name){
             return INPerson(personHandle: personHandle, nameComponents: nameComponents, displayName: nameComponents.familyName, image: nil, contactIdentifier: nil, customIdentifier: nil)
